88bc2cb2a66639c5c3e4b17001977e473ecbbbec
Shail K Patel <114606340+ShailKPatel@users.noreply.github.com>
2024-07-13 00:03:00 +0530

    Create DiscountPass.java

---
+++ DiscountPass.java
@@ -0,0 +1,44 @@
+class DiscountPass {
+    private int passId;
+    private String passType;
+    private double discountPercentage;
+
+    public DiscountPass(int passId, String passType, double discountPercentage) {
+        this.passId = passId;
+        this.passType = passType;
+        this.discountPercentage = discountPercentage;
+    }
+
+    public int getPassId() {
+        return passId;
+    }
+
+    public void setPassId(int passId) {
+        this.passId = passId;
+    }
+
+    public String getPassType() {
+        return passType;
+    }
+
+    public void setPassType(String passType) {
+        this.passType = passType;
+    }
+
+    public double getDiscountPercentage() {
+        return discountPercentage;
+    }
+
+    public void setDiscountPercentage(double discountPercentage) {
+        this.discountPercentage = discountPercentage;
+    }
+
+    @Override
+    public String toString() {
+        return "DiscountPass{" +
+                "passId=" + passId +
+                ", passType='" + passType + '\'' +
+                ", discountPercentage=" + discountPercentage +
+                '}';
+    }
+}
