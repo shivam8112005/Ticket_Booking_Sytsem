4da7a60d2006e6883dfae9c29ba4cc2f489378d0
shivam shukla <157223041+shivam8112005@users.noreply.github.com>
2024-07-16 21:59:21 +0530

    Update Customer.java

--- person/Customer.java
+++ person/Customer.java
@@ -1,31 +1,23 @@
+import java.util.HashMap;
+import java.util.LinkedList;
+
+
 class Customer extends User {
-    private String username;
     private String password;
-
-    private static int customerIdCounter = 1; // Static counter for unique IDs
-
-    public Customer(String name, String phoneNumber, Ticket ticket, String dateOfBirth, DiscountPass discountPass, String username, String password) {
-        super(customerIdCounter++, name, phoneNumber, ticket, dateOfBirth, discountPass);
-        this.username = username;
-        this.password = password;
-    }
-
-    public String getUsername() {
-        return username;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
+     static int customerId = 1; // Static counter for unique IDs
+    private LinkedList<Ticket> boookedTicket=new LinkedList<>();
+    LinkedList<User> users=new LinkedList<>();
+    HashMap<Customer,LinkedList<Passenger>> customersPassenger=new HashMap<>();
+    public Customer() {
+        super();
+    } 
     @Override
     public String toString() {
         return "Customer{" +
-                "id=" + getId() +
+                "id=" + getUserId() +
                 ", name='" + getName() + '\'' +
                 ", phoneNumber='" + getPhoneNumber() + '\'' +
                 ", dateOfBirth='" + getDateOfBirth() + '\'' +
-                ", username='" + username + '\'' +
                 '}';
     }
 }
