56a1a71ed7c663b3fc8eff580c80c713174b56c7
shivam8112005 <shivamshukla8112005@gmail.com>
2024-07-17 15:05:03 +0530

    login and signup menu in customer menu class

--- menu/CustomerMenu.java
+++ menu/CustomerMenu.java
@@ -1,10 +1,52 @@
 import java.util.*;
 public class CustomerMenu extends Customer{
+    User ul;
     Scanner sc=new Scanner(System.in);
+    int choice;
+     public CustomerMenu(int a){
+        super(10);
+     }
+    public static void main(String[] args) {
+       
+        CustomerMenu cm=new CustomerMenu(10);
+        cm.signUpMenu();
+    }
+    public  void signUpMenu() {
+        boolean exit = true;
+        
+        while (exit) {
+            System.out.println(users);
+       
+         System.out.println();
+            System.out.println("User Menu:");
+            System.out.println("1. Sign Up");
+            System.out.println("2. Login");
+            System.out.println("3. Return");
+
+            System.out.print("Enter your choice: ");
+             choice = sc.nextInt();
+            
+
+            switch (choice) {
+                case 1:
+                   customerLogIn();
+                    break;
+                case 2:
+                   customerLogIn();
+                    break;
+                case 3:
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
+
     public void customerLogIn(){
         while (true) {
             System.out.print("Enter email: ");
-            String input = sc.nextLine();
+            String input = sc.next();
             if (isValidEmail(input)) {
                 findUserByEmail(input);
                 break;
@@ -14,14 +56,17 @@ public class CustomerMenu extends Customer{
     }
     private void findUserByEmail(String input) {
         boolean b=false;
-       for(User u:users){
-        if(u.getEmail().equals(input)){
+       
+        if(users.containsKey(input)){
             b=true;
+            if(choice==1){
+                System.out.println("Email already exists!");
+            }else{
             while(true){
             System.out.println("enter password: ");
             String password=sc.next();
-            if(password.equals(u.getPassword())){
-                User ul=u;
+            if(password.equals(users.get(input).getPassword())){
+                ul=users.get(input);
                 customermenu();
                 break;
             }else{
@@ -31,18 +76,31 @@ public class CustomerMenu extends Customer{
                if(choice.equalsIgnoreCase("yes")){
                 System.out.println("Enter UserId: ");
                 int userid=sc.nextInt();
-                if(userid==u.getUserId()){
+                if(userid==users.get(input).getUserId()){
                    setPassword();
+                   System.out.println("Password Updated successfully");
+                   break;
                 }else{
                     System.out.println("incorrect user id! login failed");
+                    break;
                 }
                }
             }
+        }}
+        }
+       if(!b){
+        if(choice==1){
+            this.ul=new Customer(input);
+            
+        }
+        else{
+            System.out.println("User Not found!");
+        System.out.println("Don't Have an Account? want to sign up ? (y/n):");
+        String ch=sc.next();
+        if(ch.equalsIgnoreCase("y")){
+            this.ul=new Customer(input);
         }
         }
-       }if(!b){
-        User us=new Customer();
-        users.add(us);
        }
     }
     private void customermenu() {
--- person/Customer.java
+++ person/Customer.java
@@ -6,10 +6,12 @@ class Customer extends User {
     private String password;
      static int customerId = 1; // Static counter for unique IDs
     private LinkedList<Ticket> boookedTicket=new LinkedList<>();
-    LinkedList<User> users=new LinkedList<>();
     HashMap<Customer,LinkedList<Passenger>> customersPassenger=new HashMap<>();
-    public Customer() {
-        super();
+    public Customer(int a){
+        super(a);
+    }
+    public Customer(String email) {
+        super(email);
     } 
     @Override
     public String toString() {
