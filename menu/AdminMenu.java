656bbc733ab643d260fb8a81538a25d0a23f0b5b
Shail K Patel <114606340+ShailKPatel@users.noreply.github.com>
2024-07-17 06:04:45 +0530

    Update AdminMenu.java

--- menu/AdminMenu.java
+++ menu/AdminMenu.java
@@ -1,3 +1,6 @@
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 public class AdminMenu {
@@ -64,8 +67,9 @@ public class AdminMenu {
             scanner.nextLine();
             switch (choice) {
                 case 1:
-                    break;
+                    discountPassMenu();
                 case 2:
+                    tripMenu();
                     break;
                 case 3:
                     busMenu();
@@ -77,11 +81,193 @@ public class AdminMenu {
                     System.out.println("Logged out successfully.");
                     return;
                 case 6:
-                    return;
+                    break;
                 default:
                     System.out.println("Invalid choice. Please try again.");
             }
         }
+    }
+
+    public void discountPassMenu() {
+        boolean exit = false;
+        while (!exit) {
+            System.out.println("Discount Pass Menu:");
+            System.out.println("1. View All Discount Passes");
+            System.out.println("2. Add Discount Pass");
+            System.out.println("3. Delete Discount Pass");
+            System.out.println("4. Alter Discount Pass");
+            System.out.println("5. Return");
+
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine();
+
+            switch (choice) {
+                case 1:
+                    DiscountPass.printAllDiscountPass();
+                    break;
+                case 2:
+                    new DiscountPass();
+                    break;
+                case 3:
+                    DiscountPass.printAllDiscountPass();
+
+                    System.out.print("Enter the Pass ID to delete: ");
+                    int deleteId = scanner.nextInt();
+                    scanner.nextLine();
+                    DiscountPass passToRemove = null;
+                    for (DiscountPass pass : DiscountPass.getAllDiscountPass()) {
+                        if (pass.getPassId() == deleteId) {
+                            passToRemove = pass;
+                            break;
+                        }
+                    }
+                    if (passToRemove != null) {
+                        DiscountPass.getAllDiscountPass().remove(passToRemove);
+                        System.out.println("Discount Pass removed successfully.");
+                    } else {
+                        System.out.println("Pass ID not found.");
+                    }
+                    break;
+                case 4:
+                    DiscountPass.printAllDiscountPass();
+                    System.out.print("Enter the Pass ID to alter: ");
+                    int alterId = scanner.nextInt();
+                    scanner.nextLine();
+                    for (DiscountPass pass : DiscountPass.getAllDiscountPass()) {
+                        if (pass.getPassId() == alterId) {
+                            boolean alterExit = false;
+                            while (!alterExit) {
+                                System.out.println("Pass ID: " + pass.getPassId());
+                                System.out.println("1. Change Pass Name");
+                                System.out.println("2. Change Discount Percentage");
+                                System.out.println("3. Return");
+
+                                System.out.print("Enter your choice: ");
+                                int alterChoice = scanner.nextInt();
+                                scanner.nextLine();
+
+                                switch (alterChoice) {
+                                    case 1:
+                                        pass.setPassName();
+                                        break;
+                                    case 2:
+                                        pass.setDiscountPercentage();
+                                        break;
+                                    case 3:
+                                        alterExit = true;
+                                        break;
+                                    default:
+                                        System.out.println("Invalid choice. Please try again.");
+                                }
+                            }
+                            break;
+                        }
+                    }
+                    break;
+                case 5:
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+    }
+
+    public void tripMenu() {
+        boolean exit = false;
+        while (!exit) {
+            System.out.println("Trip Menu:");
+            System.out.println("1. View All Trips");
+            System.out.println("2. View All Pending Trips");
+            System.out.println("3. View All Ended Trips");
+            System.out.println("4. Add Trip");
+            System.out.println("5. Add Multiple Trips with Fixed Start and End Location");
+            System.out.println("6. Return");
+
+            System.out.print("Enter your choice: ");
+            int choice = scanner.nextInt();
+            scanner.nextLine(); // Consume newline
+
+            switch (choice) {
+                case 1:
+                    Trip.printAllTrip();
+                    break;
+                case 2:
+                    LocalDateTime currentTime = LocalDateTime.now(); // Get the current time
+                    for (Trip trip : Trip.getAllTrip()) {
+                        if (trip.getEndTime().isAfter(currentTime)) {
+                            System.out.println("Trip ID: " + trip.getTripId() +
+                                    ", Start Location: " + trip.getStartLocation() +
+                                    ", Start Time: " + trip.getStartTime() +
+                                    ", End Location: " + trip.getEndLocation() +
+                                    ", End Time: " + trip.getEndTime());
+                        }
+                    }
+                    break;
+                case 3:
+                    currentTime = LocalDateTime.now(); // Get the current time
+                    for (Trip trip : Trip.getAllTrip()) {
+                        if (trip.getEndTime().isBefore(currentTime)) {
+                            System.out.println("Trip ID: " + trip.getTripId() +
+                                    ", Start Location: " + trip.getStartLocation() +
+                                    ", Start Time: " + trip.getStartTime() +
+                                    ", End Location: " + trip.getEndLocation() +
+                                    ", End Time: " + trip.getEndTime());
+                        }
+                    }
+                    break;
+                case 4:
+                    new Trip();
+                    break;
+                case 5:
+
+                    System.out.print("Enter start location: ");
+                    String startLocation = scanner.nextLine();
+
+                    System.out.print("Enter end location: ");
+                    String endLocation = scanner.nextLine();
+
+                    int numberOfTrips = 0;
+                    while (numberOfTrips <= 0) {
+                        System.out.print("How many trips do you want to enter (must be greater than 0): ");
+                        numberOfTrips = scanner.nextInt();
+                        scanner.nextLine(); // Consume newline
+                    }
+
+                    List<Trip> newTrips = new ArrayList<>(); // List to hold newly created trips
+
+                    for (int i = 0; i < numberOfTrips; i++) {
+                        System.out.println("Entering details for trip " + (i + 1) + ":");
+
+                        LocalDateTime startTime = Trip.setTime("Enter start time (format: yyyy-MM-dd HH:mm): ");
+                        LocalDateTime endTime = Trip.setTime("Enter end time (format: yyyy-MM-dd HH:mm): ");
+
+                        // Create a new trip with the given details
+                        Trip trip = new Trip(startLocation, startTime, endLocation, endTime);
+                        newTrips.add(trip); // Add the new trip to the list
+                    }
+
+                    // Print only the newly added trips
+                    System.out.println("Newly added trips:");
+                    for (Trip trip : newTrips) {
+                        System.out.println("Trip ID: " + trip.getTripId() +
+                                ", Start Location: " + trip.getStartLocation() +
+                                ", Start Time: " + trip.getStartTime() +
+                                ", End Location: " + trip.getEndLocation() +
+                                ", End Time: " + trip.getEndTime());
+
+                    }
+
+                    break;
+                case 6:
+                    exit = true;
+                    break;
+                default:
+                    System.out.println("Invalid choice. Please try again.");
+            }
+        }
+
     }
 
     public void busMenu() {
