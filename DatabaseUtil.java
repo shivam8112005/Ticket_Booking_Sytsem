ba3bfc6af25da7fde4ca6071ba4546b244362ebb
Prince3255 <149878886+Prince3255@users.noreply.github.com>
2024-07-17 13:17:13 +0530

    Update DatabaseUtil.java

--- DatabaseUtil.java
+++ DatabaseUtil.java
@@ -1,8 +1,11 @@
 import java.sql.*;
 
 public class DatabaseUtil {
-    public static void main(String[] args) throws Exception {
-        Connection con = DriverManager.getConnection("jdbc:mysql://localhost:3306/ticket_booking_system", "root", "");
-        System.out.println(con != null ? "success" : "error");
+    private static final String URL = "jdbc:mysql://localhost:3306/ticket_booking_system";
+    private static final String USER = "root";
+    private static final String PASSWORD = "";
+    public static Connection getConnection() throws Exception {
+        Class.forName("com.mysql.cj.jdbc.Driver");
+        return DriverManager.getConnection(URL, USER, PASSWORD);
     }
 }
